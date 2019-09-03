fda271dc4eca48b8bf6b63d167d697fdc2807f9c
unknown <eljohn.bonga@g.msuiit.edu.ph>
2019-09-03 11:11:47 +0800

    added an add comment feature

--- djangoProj/urls.py
+++ djangoProj/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hi/', include('hello.urls'))
+    path('hi/', include('hello.urls')),
+    path('guestbook/', include('guestbook.urls'))
 ]
--- guestbook/views.py
+++ guestbook/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Comment
+
+def index(request):
+    comments = Comment.objects.order_by('-date_added')
+    context = {'comments': comments} 
+    return render(request, 'guestbook/index.html', context)
+
+def sign(request):
+    return render(request, 'guestbook/sign.html')
